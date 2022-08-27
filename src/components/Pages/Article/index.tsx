7601cf9c30b866b5c1901d58877875dd50bbe14f
phyauab <phyauab@connect.ust.hk>
2022-08-27 10:35:00 +0800

    update article meta

--- src/components/Pages/Article/index.tsx
+++ src/components/Pages/Article/index.tsx
@@ -65,26 +65,14 @@ export function ArticlePage() {
         <hr />
 
         <div className="article-actions">
-          <div className="article-meta">
-            <a href="#">
-              <img src="http://i.imgur.com/Qr71crq.jpg" />
-            </a>
-            <div className="info">
-              <a href="" className="author">
-                Eric Simons
-              </a>
-              <span className="date">January 20th</span>
-            </div>
-            <button className="btn btn-sm btn-outline-secondary">
-              <i className="ion-plus-round"></i>
-              &nbsp; Follow Eric Simons
-            </button>
-            &nbsp;
-            <button className="btn btn-sm btn-outline-primary">
-              <i className="ion-heart"></i>
-              &nbsp; Favorite Post <span className="counter">(29)</span>
-            </button>
-          </div>
+          <ArticleMeta
+            slug={slug}
+            title={article.title}
+            author={article.author}
+            createdAt={article.createdAt}
+            favorited={article.favorited}
+            favoritesCount={article.favoritesCount}
+          />
         </div>
 
         <div className="row">
